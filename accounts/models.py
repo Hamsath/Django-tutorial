d1086e75c92f99015c0b5e41a76bacb945d8d563
Hamsath <mukunthuhamsath@gmail.com>
2018-02-27 19:22:07 +0530

    Updated code

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -12,3 +13,9 @@ class userProfile(models.Model):
     city=models.CharField(max_length=20,default='')
     website=models.URLField(default='')
     phone=models.IntegerField(default=0)
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = userProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile,sender=User)
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -13,3 +13,15 @@ def home(request):
 
     args = {'myName':myName,'numbers':numbers}
     return render(request,'accounts/home.html',args)
+
+def register(request):
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+
+    else:
+        form = UserCreationForm()
+        args={'form':form}
+        return render(request, 'accounts/reg_form.html',args)
