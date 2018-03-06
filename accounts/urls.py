1b1c02e30d93abd309b529f4368fce4aa4e95ecd
Hamsath <mukunthuhamsath@gmail.com>
2018-03-06 19:03:27 +0530

    Added a middleware class for redirects corresponding to authenticated and non-authenticated users

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,24 @@
 from django.conf.urls import url
 from . import views                     # using . will search in outer tutorial directory
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.views import (login,
+                    logout,
+                    password_reset,
+                    password_reset_done,
+                    password_reset_confirm,
+                    password_reset_complete
+                    )
 
 urlpatterns=[
-    url(r'^$',views.home),
-    url(r'^login/$',login,{'template_name':'accounts/login.html'}),
-    url(r'^logout/$',login,{'template_name':'accounts/logout.html'}),
+    url(r'^$',views.home,name='home'),
+    url(r'^login/$',login,{'template_name':'accounts/login.html'},name='login'),
+    url(r'^logout/$',logout,{'template_name':'accounts/logout.html'},name='logout'),
     url(r'^register/$',views.register,name='register'),
     url(r'^profile/$',views.view_profile,name='view_profile'),
     url(r'^profile/edit/$',views.edit_profile,name='edit_profile'),
+    url(r'^change_password/$',views.change_password,name='change_password'),
+    url(r'^password_reset/$', password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password_reset/complete/$', password_reset_complete, name='password_reset_complete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render,redirect
 from accounts.forms import RegistrationForm,EditProfileForm
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserChangeForm   # Default form to edit user data
-
+from django.contrib.auth.forms import UserChangeForm,PasswordChangeForm   # Default form to edit user data
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -28,9 +29,11 @@ def register(request):
         args={'form':form}
         return render(request, 'accounts/reg_form.html',args)
 
+
 def view_profile(request):
     args = {'user':request.user}
     return render(request,'accounts/profile.html',args)
+
 
 def edit_profile(request):
     if request.method == 'POST':
@@ -44,3 +47,18 @@ def edit_profile(request):
         form = EditProfileForm(instance = request.user)
         args={'form':form}
         return render(request, 'accounts/edit_profile.html',args)
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data = request.POST, user = request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('/account/profile')
+
+    else:
+        form = PasswordChangeForm(user = request.user)
+        args={'form':form}
+        return render(request, 'accounts/change_password.html',args)
