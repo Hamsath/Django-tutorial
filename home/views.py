261cfcedc9ea53e95746f11f81c0b15cb3c223d1
Hamsath <mukunthuhamsath@gmail.com>
2018-03-19 15:45:09 +0530

    Added a new home app and added a customized field to check how posting data works

--- home/views.py
+++ home/views.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from home.forms import HomeForm
 
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('It should work')
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
+
+    def get(self, request):
+        form = HomeForm()
+        return render(request,self.template_name,{'form':form})
+
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['post']
+            form = HomeForm()
+        args = {'form':form , 'text':text }
+
+        return render(request,self.template_name,args)
