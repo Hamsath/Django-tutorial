aa3d66586159eebd2d7bbb815e8b81053315bd4c
Hamsath <mukunthuhamsath@gmail.com>
2018-03-22 18:29:39 +0530

    Made post data to get saved in DB model

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
 from home.forms import HomeForm
+from home.models import Post
 
 
 # Create your views here.
@@ -12,13 +13,19 @@ class HomeView(TemplateView):
 
     def get(self, request):
         form = HomeForm()
-        return render(request,self.template_name,{'form':form})
+        posts = Post.objects.all()
+        args = {'form':form, 'posts': posts}
+        return render(request,self.template_name,args)
 
     def post(self, request):
         form = HomeForm(request.POST)
         if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
             text = form.cleaned_data['post']
             form = HomeForm()
-        args = {'form':form , 'text':text }
+            return redirect('home:home')
 
+        args = {'form':form , 'text':text }
         return render(request,self.template_name,args)
