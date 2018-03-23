c26170deb52133aa8bec0ad914ed1b2889f5952d
Hamsath <mukunthuhamsath@gmail.com>
2018-03-23 19:12:39 +0530

    User posting status and displaying other users in the home page

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,10 +9,12 @@ from django.contrib.auth.views import (login,
                     )
 
 urlpatterns=[
+    url(r'^$',views.view_profile,name='view_profile'),
     url(r'^login/$',login,{'template_name':'accounts/login.html'},name='login'),
     url(r'^logout/$',logout,{'template_name':'accounts/logout.html'},name='logout'),
     url(r'^register/$',views.register,name='register'),
     url(r'^profile/$',views.view_profile,name='view_profile'),
+    url(r'^profile/(?P<pk>\d+)$',views.view_profile,name='view_profile_by_pk'),
     url(r'^profile/edit/$',views.edit_profile,name='edit_profile'),
     url(r'^change_password/$',views.change_password,name='change_password'),
 
--- home/models.py
+++ home/models.py
@@ -9,7 +9,12 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     post = models.CharField(max_length = 200)
     user = models.ForeignKey(User)
-    date = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
 
     def __str__(self):
         return self.post
+
+class Friend(models.Model):
+    users = models.ManyToManyField(User)
+    
